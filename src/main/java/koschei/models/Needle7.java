d72576ca63b2390f32dc734265f63049396e121b
Dzhavid <eldarov.dzhavid@mail.ru>
2020-06-25 23:02:46 +0300

    1-t commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Needle7 needle7;
+
+    public Needle7 getNeedle7() {
+        return needle7;
+    }
+
+    @Autowired
+    public void setNeedle7(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
 
     @Override
     public String toString() {
--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,5 +1,6 @@
 package koschei.models;
 
+import koschei.KoscheiTheDeathless;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,9 @@ public class Ocean1 {
 
     @Autowired
     private Island2 island;
+
+    @Autowired
+    private KoscheiTheDeathless koscheiTheDeathless;
 
     @Override
     public String toString() {
